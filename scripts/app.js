0afe7d238ade8ad04d1bba859eee4303f2554335
Luke Elliott <revlucio@gmail.com>
2014-12-07 22:21:16 +0000

    adding basic modal support - still requires styling

--- scripts/app.js
+++ scripts/app.js
@@ -9,4 +9,15 @@ function NavController() {
 	var vm = this;
 
 	vm.state = '';
+
+	vm.showPopup = false;
+	vm.imgSrc = 'fish';
+	vm.title = 'crazy monsters - 2014'
+
+	vm.select = select;
+
+	function select(src) {
+		vm.showPopup = true;
+		vm.imgSrc = src;
+	}
 }
