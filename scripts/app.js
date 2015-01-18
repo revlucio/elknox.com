3c3ff3a948f5e5cf164f2e8db0b4044787bacce0
Luke Elliott <luke.elliott@blackthorn.com>
2015-01-18 21:45:24 +0000

    updated images and added a splash screen

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
 angular
-	.module('app', []);
+	.module('app', ['ngAnimate']);
 
 angular
 	.module('app')
 	.controller('NavController', NavController);
 
-function NavController() {
+function NavController($timeout) {
 	var vm = this;
 
 	vm.state = 'home';
@@ -13,6 +13,15 @@ function NavController() {
 	vm.showPopup = false;
 	vm.imgSrc = 'fish';
 	vm.title = 'crazy monsters - 2014'
+
+	vm.isIntroDone = false;
+
+	$timeout(endIntro, 2000);
+
+	function endIntro() {
+		vm.isIntroDone = true;
+	}
+
 
 	//vm.select = select;
 
