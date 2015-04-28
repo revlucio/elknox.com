1ef938b21abfbae5a15444b8f8ff0d28b38fecd7
Luke Elliott <revlucio@gmail.com>
2015-04-28 22:28:38 +0100

    removing old bundle files as going to use gulp now

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -9,29 +9,19 @@ var filename = 'app.' + Date.now() + '.';
 gulp.task('default', ['clean:dist', 'build']);
 
 gulp.task('build', function() {
-	gulp.src('src/**/*.html')
-        .pipe(templateCache({module: 'speaker-tracker'}))
-        .pipe(gulp.dest('src'));
 
 	gulp.src([
-			'vendor/angular-1.4.0-beta4.min.js', 
-			'src/module.js', 
-			'src/**/*.js'])
+            'scripts/vendor/angular-1.3.3.js', 
+			'scripts/vendor/angular-animate1.3.3.js',
+			'scripts/**/*.js'])
     	.pipe(concat(filename + 'js'))
-    	.pipe(gulp.dest('dist'));
+    	.pipe(gulp.dest('../dist'));
 
     gulp.src('index.html')
     	.pipe(replace('app.', filename))
     	.pipe(gulp.dest('dist'));
-
-	gulp.src(['content/**/*.css', 'vendor/**/*.css'])
-		.pipe(concat(filename + 'css'))
-    	.pipe(gulp.dest('dist'));
-
-    gulp.src('vendor/fonts/*.woff')
-    	.pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('clean:dist', function() {
-	return del('dist/**/*');
+	return del('dist/**/*.js');
 });
